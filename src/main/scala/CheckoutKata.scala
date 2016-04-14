59a31a09818f5262f8b91c0edc9703bc4a0e8af6
Colm <colm.m.cavanagh@gmail.com>
2016-04-15 00:54:09 +0100

    Frist Commit

---
+++ src/main/scala/CheckoutKata.scala
@@ -0,0 +1,34 @@
+/**
+ * Created by Colm on 14/04/2016.
+ */
+
+class CheckoutKata(itemRules:List[ItemRules]) {
+
+  val itemsRulesAsMap = itemRules.map(ir => ir.SKU -> ir).toMap
+  var Items = scala.collection.mutable.ListBuffer.empty[String]
+
+  def scanItem(item: String) = Items += item
+
+  def getTotal(): Double = {
+    calculateTotalPrice(Items.toList)
+  }
+
+  def calculateTotalPrice(items: List[String]) = {
+    val itemsWithRulesToCount = items.map(i => itemsRulesAsMap(i))
+      .groupBy(i => i).map(ir => (ir._1, ir._2.length))
+    itemsWithRulesToCount.map(k => k._1 match {
+      case ItemRules(_, up, None) => up * k._2
+      case ItemRules(_, up, Some(sr)) =>calculateDiscountedPrice(sr,k._2,up)
+    }).sum
+  }
+
+  def calculateDiscountedPrice(itemSpecialRules: SpecialRules,
+                               quantityOfItems: Int,
+                               unitPrice:Double): Double = {
+     val discountedGoods = quantityOfItems / itemSpecialRules.quanityForSpecialDiscount *  itemSpecialRules.setPrice
+    discountedGoods  + (quantityOfItems % itemSpecialRules.quanityForSpecialDiscount ) * unitPrice
+  }
+}
+
+case class ItemRules(SKU:String,unitPrice:Double,specialPrice:Option[SpecialRules])
+case class SpecialRules(quanityForSpecialDiscount:Int,setPrice:Double)
---
+++ src/test/scala/CheckoutKataTest.scala
@@ -0,0 +1,48 @@
+/**
+ * Created by Colm on 14/04/2016.
+ */
+
+import org.scalatest._
+
+class CheckoutKataTest extends FlatSpec {
+
+  val testRulesList: List[ItemRules] = List(ItemRules("A", 50.0, Some(SpecialRules(3, 130))),
+    ItemRules("B", 30.0, Some(SpecialRules(3, 300))), ItemRules("C", 20.0, None),
+    ItemRules("D", 15.0, None))
+
+  val checkoutKataTest = new CheckoutKata(testRulesList)
+
+  "checkoutTransaction" should "return zero if their are no items" in {
+    assert(checkoutKataTest.getTotal() == 0)
+  }
+  "checkoutTransaction" should "return the item price" in {
+    checkoutKataTest.scanItem("C")
+    assert(checkoutKataTest.getTotal() == 20.0)
+  }
+  "checkoutTransaction" should "sum the item prices up with no speical rules" in {
+    checkoutKataTest.scanItem("D")
+    assert(checkoutKataTest.getTotal() == 35.0)
+  }
+  "checkoutTransaction" should "should  apply the special rules if they apply" in {
+    checkoutKataTest.scanItem("A")
+    checkoutKataTest.scanItem("A")
+    checkoutKataTest.scanItem("A")
+    assert(checkoutKataTest.getTotal() == 165.0)
+  }
+  "checkoutTransaction" should "should not apply the special rules if they did not meet it" in {
+    checkoutKataTest.scanItem("A")
+    assert(checkoutKataTest.getTotal() == 215.0)
+  }
+  "checkoutTransaction" should "should not care what order they put in " in {
+    checkoutKataTest.scanItem("B")
+    checkoutKataTest.scanItem("B")
+    checkoutKataTest.scanItem("A")
+    checkoutKataTest.scanItem("A")
+    checkoutKataTest.scanItem("B")
+    checkoutKataTest.scanItem("A")
+
+    assert(checkoutKataTest.getTotal() == 215.0)
+  }
+
+
+}
